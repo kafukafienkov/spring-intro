c3f766830084d3dfbbc14d9b3f8d40fec882474c
kafukafienkov <bartekjmroz@gmail.com>
2015-05-31 22:30:31 +0200

    Initial commit

---
+++ C01_Spring_Container_1/src/main/java/Runner.java
@@ -0,0 +1,13 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * author: Bartek
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("context.xml");
+    }
+}
