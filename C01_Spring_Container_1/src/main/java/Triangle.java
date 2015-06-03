d3ec15d6e2de24bf53a54b273460db1bd50f804c
kafukafienkov <bartekjmroz@gmail.com>
2015-06-03 14:20:21 +0200

    Initialization of an object using constructor

--- C01_Spring_Container_1/src/main/java/Triangle.java
+++ C01_Spring_Container_1/src/main/java/Triangle.java
@@ -7,8 +7,40 @@ public class Triangle {
 
     static Logger log = Logger.getLogger(Triangle.class.getName());
 
+    private String type;
+    private int height;
+
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+
+    public Triangle(String type) {
+        this.type = type;
+    }
+
+    public Triangle(int height) {
+        this.height = height;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
     public void draw() {
-        System.out.println("Triangle drawn");
+        System.out.println(getType() + " Triangle drawn of height: " + getHeight());
         log.info("A triangle has just been drawn");
     }
 }
